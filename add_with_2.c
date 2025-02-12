aacea069202ffe9da2611874c2e06c02a00c1ece
unknown <jayasrithimmaraj4@gmail.com>
2025-02-12 15:57:42 +0530

    multiply3

---
+++ add_with_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x,add;
+    printf("enter the num:");
+    scanf("%d",&x);
+    add= x+2;
+    printf("the new after adding the given value with 2:%d",add);
+    return 0;
+}
