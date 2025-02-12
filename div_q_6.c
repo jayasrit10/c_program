a79041d9e17b74ba41ec369ca2e1aef4e9eaceae
unknown <jayasrithimmaraj4@gmail.com>
2025-02-12 16:26:43 +0530

    div6

---
+++ div_q_6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x,div;
+    printf("enter the num:");
+    scanf("%d",&x);
+    div= x/6;
+    printf("the new after dividing the given value with 6:%d",div);
+    return 0;
+}
