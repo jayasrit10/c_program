948b15b0b5574ba65caaae4149f3c581fdba7c58
unknown <jayasrithimmaraj4@gmail.com>
2025-02-12 15:45:20 +0530

    multiply3

---
+++ multi_with_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float x, product;
+    printf("enter any value:");
+    scanf("%f",&x);
+    product=x*3;
+    printf("the multiplied with 3 is:%.2f",product);
+    return 0;
+}
+
