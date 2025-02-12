7a3c84cc485b4611dc4dcfd158f760cc533f73ad
unknown <jayasrithimmaraj4@gmail.com>
2025-02-12 16:13:21 +0530

    sub5

---
+++ sub_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float x,sub;
+    printf("enter the num:");
+    scanf("%f",&x);
+    sub= x-5;
+    printf("the new after subtracting the given value with 2:%.2f",sub);
+    return 0;
+}
